5ac0c4704db4bddb9dc94468e171e72742076a7e
Nicolas Mopin <Nicolas.mopin@edu.devinci.fr>
2022-03-27 20:32:46 +0200

    :art: Twitter design

--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -211,10 +211,10 @@
                             <img class="inline-block h-10 w-10 rounded-full" src="https://pbs.twimg.com/profile_images/1121328878142853120/e-rpjoJi_bigger.png" alt="">
                         </div>
                         <div class="ml-3">
-                            <p class="text-base leading-6 font-medium text-white">
-                                Sonali Hirave
+                            <p class="text-base font-bold leading-6 font-medium text-white">
+                                Email
                                 <span class="text-sm leading-5 font-medium text-gray-400 group-hover:text-gray-300 transition ease-in-out duration-150">
-                                    @ShonaDesign  . 16 April
+                                    • Date
                                 </span>
                             </p>
                         </div>
@@ -224,19 +224,11 @@
 
 
             <div class="pl-16">
-                <p class="text-base width-auto font-medium text-white flex-shrink">
-                    Day 07 of the challenge <a href="#" class="text-blue-400">#100DaysOfCode</a>
-                    I was wondering what I can do with <a href="#" class="text-blue-400">#tailwindcss</a>, so just started building
-                    Twitter UI using Tailwind and so far it looks so promising. I will post my code after completion.
-                    [07/100]
-                    <a href="#" class="text-blue-400"> #WomenWhoCode #CodeNewbie</a>
+                <p class="text-left text-base width-auto font-medium text-white flex-shrink">
+                    content
                 </p>
 
-                <div class="md:flex-shrink pr-6 pt-3">
-                    <div class="bg-cover bg-no-repeat bg-center rounded-lg w-full h-64" style="height: 200px; background-image: url(https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=448&amp;q=80);">
-                    <img class="opacity-0 w-full h-full" src="https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=448&amp;q=80" alt="">
-                </div>
-                </div>
+
 
 
                 <div class="flex items-center py-4">
