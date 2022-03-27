4d6f99a6cb68a03b4bd66770ac4db79a574805d9
Nicolas Mopin <Nicolas.mopin@edu.devinci.fr>
2022-03-27 23:31:39 +0200

    :sparkles: SignIn & SignUp style

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,34 +1,27 @@
 <template>
-  <div>
-    <div class="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-      <div class="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign up</h2>
-      </div>
-      <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <div class="space-y-6">
-            <div>
-              <label for="email" class="block text-left text-sm font-medium text-gray-700"> Email address </label>
-              <div class="mt-1">
-                <input id="email" v-model="email" name="email" type="email" autocomplete="email" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-              </div>
-            </div>
+  <div class="w-full h-full" style="background-color: #15202b;">
+    <div class="flex w-screen h-screen items-center justify-center">
+        <div class="min-h-1/2 border border-gray-900 rounded-2xl border-2 border-white">
 
-            <div>
-              <label for="password" class="block text-sm text-left font-medium text-gray-700"> Password </label>
-              <div class="mt-1">
-                <input id="password" v-model="password" name="password" type="password" autocomplete="current-password" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-              </div>
-            </div>
+            <div class="mx-4 sm:mx-24 md:mx-34 lg:mx-56 mx-auto  flex items-center space-y-4 py-16 font-semibold text-gray-500 flex-col">
+                <svg viewBox="0 0 24 24" class=" h-12 w-12 text-white transform rotate-180" fill="currentColor">
+                    <g>
+                        <path
+                            d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z">
+                        </path>
+                    </g>
+                </svg>
 
-            <div class="flex items-center justify-between">
-              <div class="text-sm">
-                <button @click="goTo('signIn')" type="button" class="font-medium text-indigo-600 hover:text-indigo-500"> Already a member ? Log in here </button>
-              </div>
-            </div>
+                <h1 class="text-white text-2xl">Sign up to Tertwi</h1>
+                <input id="email"  v-model="email" name="email" placeholder="Mail" type="email" autocomplete="email" required class="w-full p-2 rounded-md  border border-gray-700 focus:border-blue-700" style="background-color: #15202b;">
+                <input id="password" v-model="password" name="password" type="password" autocomplete="current-password" required class="w-full p-2 rounded-md border border-gray-700" placeholder="Password" style="background-color: #15202b;">
+                <button @click="createUser()" type="submit" class="w-full p-2 bg-gray-50 rounded-full font-bold text-gray-900 border border-gray-700 ">Sign up</button>
 
-            <div>
-              <button @click="createUser()" type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Sign up</button>
+                <p>Already a member ?
+                    <button @click="goTo('signIn')" type="button" class="font-semibold text-sky-700">Log in</button>
+                </p>
+
+
             </div>
 
             <div v-if="error" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
@@ -36,11 +29,10 @@
               <span class="block sm:inline">{{ error.message }}</span>
             </div>
 
-          </div>
         </div>
-      </div>
     </div>
   </div>
+
 </template>
 
 <script>
